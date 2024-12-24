ed5e59b9ca1acf6110eec29e1b3aa0b051777533
Beatriz Marques <beatrizqmarques@gmail.com>
2024-12-24 17:03:50 -0300

    Add files via upload

---
+++ Python/Atividade_03.py
@@ -0,0 +1,7 @@
+num1 = input ('Entre com um número:')
+num2 = input ('Entre com um número:')
+soma = int(num1) + int(num2)
+
+#print ('A soma entre', num1,'e', num2, 'é:', soma)
+print ('A soma entre {} e {} é: {}' .format(num1,num2,soma))
+
---
+++ Python/Atividade_04.py
@@ -0,0 +1,9 @@
+var = str(input('Digite algo:'))
+
+print (type(var))
+print ( var.isnumeric())
+print ( var.isalnum())
+print ( var.isalpha())
+print ( var.isdecimal())
+print ( var.islower())
+print ( var.isnumeric())
